4d2a145c1d13d18505cf1d4789b169b605ab8b45
JP Moresmau <jp@moresmau.fr>
2012-01-04 22:36:46 +0100

    InputMode test + additional checks on outputmode

--- test/Robotics/NXT/Basic.hs
+++ test/Robotics/NXT/Basic.hs
@@ -8,7 +8,7 @@ import Control.Concurrent (threadDelay)
 import Data.IORef
 
 basicTests :: IORef NXTInternals -> [Test]
-basicTests d= map (\x->x d) [testDeviceInfo,testOutputState]
+basicTests d= map (\x->x d) [testDeviceInfo,testOutputState,testInputMode]
 --
 
 delay :: IO()
@@ -33,9 +33,30 @@ testDeviceInfo ref= TestLabel "testDeviceInfo" (TestCase (do
         
 testOutputState :: IORef NXTInternals -> Test
 testOutputState ref= TestLabel "testOutputState" (TestCase (do
-       (OutputState port power modes reg ratio runstate limit count block rotation) <- testNXT ref (do
+       (OutputState port power modes reg ratio _ limit count _ _) <- testNXT ref (do
                 setOutputStateConfirm A 75 [MotorOn,Brake] RegulationModeMotorSpeed 0 MotorRunStateRunning 360
                 getOutputState A
                 )
        assertEqual "not A port" A port
+       assertEqual "not 75 power" 75 power
+       assertEqual "not modes" [MotorOn,Brake] modes
+       assertEqual "not regulation" RegulationModeMotorSpeed reg
+       assertEqual "not 0 ratio" 0 ratio
+       assertEqual "not 360 limit" 360 limit
+       assertBool "count>0" (count>0)
        ))
+       
+testInputMode :: IORef NXTInternals -> Test
+testInputMode ref= TestLabel "testInputMode" (TestCase (do
+        InputValue port valid calibrated stype smode _ normV scalV _<-testNXT ref (do
+                setInputModeConfirm One Switch BooleanMode
+                getInputValues One
+                )
+        assertEqual "not port 1" One port
+        assertBool "not valid" valid
+        assertBool "calibrated" (not calibrated)
+        assertEqual "not switch" Switch stype
+        assertEqual "not boolean" BooleanMode smode
+        assertBool "normalized not in range" (normV>=0 && normV<1024)
+        assertEqual "scaled not 0" 0 scalV
+        ))
